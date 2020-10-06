4487ea9ce194ad93ecec9ee59e38de327b77e2f2
amjadiftikhar <amjadiftikhar@github.com>
2020-10-06 22:57:34 +0200

    Movie.js edited

--- src/components/movie-catalogue.components/Movie.js
+++ src/components/movie-catalogue.components/Movie.js
@@ -1,4 +1,4 @@
-import React, {Fragment, useState} from 'react';
+import React, {useState} from 'react';
 import {Card} from 'reactstrap';
 import People from "./People";
 import "./movie.styles.css";
